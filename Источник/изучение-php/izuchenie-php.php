095984a0e7469d7b14cda183ef587e53d4c7679f
gendalf0101 <75488124+gendalf0101@users.noreply.github.com>
2021-05-24 17:50:26 +0300

    Add files via upload

---
+++ Источник/изучение-php/izuchenie-php.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'Мой сайт - Митрошин Геннадий';
+$name = 'Митрошин Геннадий';
+$image = 'img/unknown-raccoon.svg';
+$email = 'gendalf.09@yandex.ru';
+$phone = '+380994277823';
+
+require('components/header.php');
+require('components/status.php');
+require('components/about.php');
+require('components/photos.php');
+require('components/news.php');
+require('components/quotes.php');
+require('components/contacts.php');
+require('components/footer.php');
